a7d5b3c0438767ea5e828ce1fcc9588bc464f9b0
Sandro Simperl <sandrosimperl@outlook.de>
2024-07-21 11:51:29 +0200

    added main view für den editor

--- src/main/java/eu/bluefogdev/maskreplacer/EditorApplication.java
+++ src/main/java/eu/bluefogdev/maskreplacer/EditorApplication.java
@@ -1,16 +1,34 @@
 package eu.bluefogdev.maskreplacer;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class EditorApplication extends Application {
+
+    private static final Logger logger = Logger.getLogger(EditorApplication.class.getName());
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage mainStage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(EditorApplication.class.getResource("editor-view.fxml"));
 
+            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            mainStage.setTitle("Hello!");
+            mainStage.setScene(scene);
+            mainStage.show();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to load editor view\n", e);
+            mainStage.close();
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module eu.bluefogdev.maskreplacer {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
 
 
     opens eu.bluefogdev.maskreplacer to javafx.fxml;
